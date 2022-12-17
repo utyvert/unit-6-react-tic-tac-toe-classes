a9413d373ae950e0732edafc7056ffce6d966b87
Mark Bryan <marklawbryan@gmail.com>
2022-12-17 11:21:21 -0800

    added box component

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import Box from './components/Box.jsx';
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Tic Tac Toe</h1>
+        <Box letter='x' />
       </div>
     );
   }
